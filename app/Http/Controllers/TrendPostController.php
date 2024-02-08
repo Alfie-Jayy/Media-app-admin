ee6b539567bb0248db9de1fa0af81f4d41968a9e
Kudae Sithu <kudaesithu2@gmail.com>
2024-02-08 13:28:45 +0630

    query update

--- app/Http/Controllers/TrendPostController.php
+++ app/Http/Controllers/TrendPostController.php
@@ -12,6 +12,10 @@ class TrendPostController extends Controller
     public function trendPost(){
 
         $posts = actionLog::select('action_logs.*', 'posts.*', DB::raw('COUNT(action_logs.post_id) as view_count'))
+        ->when(request('key'), function($query){
+            $query
+            -> orWhere('posts.title', 'like', '%'.request('key').'%');
+        })
         ->leftJoin('posts', 'posts.post_id', 'action_logs.post_id')
         ->groupBy('action_logs.post_id')
         ->get();
